1c4747585ec17860beec5244d3da0b0e6d268174
GOΠZO <37483725+GONZOsint@users.noreply.github.com>
2021-05-07 13:18:27 +0200

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 printline() { tput setaf 3; echo "---------------------------------------------------------"; tput sgr0; };
-ip=$(ip -o addr | awk /'[0-9]{1,3}(\.[0-9]{1,3}){3}/{ print $2 "  -  " $4 }');
+ip=$(ifconfig eth0 | grep inet | awk '{ print $2 }');
 echo "";
 printline;
 tput setaf 3; echo "                   System monitor"; tput sgr0;
 printline;
 echo "";
-echo "\e[1mActual user:\e[0m\e[1;31m"  $USER "\e[0m";
-echo "\e[1mNetwork:\e[0m\e[1;34m    "  $ip;
-echo "\e[0m"
+echo -e "\e[1mActual user:\e[0m\e[1;31m"  $USER "\e[0m";
+echo -e  "\e[1mNetwork:\e[0m\e[1;34m    "  $ip;
+echo -e  "\e[0m"
 printline;
 echo "";
-echo "\e[1mLast logins:\e[0m";
+echo -e  "\e[1mLast logins:\e[0m";
 echo "";
 last -3 | head -3 | cut -c 21-;
 echo "";
 printline;
 echo "";
-echo "\e[1mDisk use:\e[0m";
+echo -e  "\e[1mDisk use:\e[0m";
 echo "";
 df -h;
 echo "";
@@ -28,14 +28,14 @@ echo "";
 echo 1 > /proc/sys/vm/drop_caches;
 echo 2 > /proc/sys/vm/drop_caches;
 echo 3 > /proc/sys/vm/drop_caches;
-echo "\e[1mMemory usage:\e[0m";
+echo -e  "\e[1mMemory usage:\e[0m";
 echo "";
 free -m | head -1 | awk '{print "          " $1 "  " $2 "  " $3}';
 free -m | grep Mem | awk '{print "          "$2"    "$3"   "$4}';
 echo "";
 printline;
 echo "";
-echo "\e[1mProcesses:\e[0m";
+echo -e  "\e[1mProcesses:\e[0m";
 echo ""
 ps;
 echo "";
